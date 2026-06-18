81a7c8547daf59483f6465a50fb9c081817c5657
Ira Alulkar <olkadotspinkp@gmail.com>
2026-06-18 19:02:16 -0400

    added color to comments

--- scripts.js
+++ scripts.js
@@ -3,10 +3,12 @@ function modeToggle() {
     if (lightMode) {
         document.documentElement.style.setProperty('--background-color', '#000000');
         document.documentElement.style.setProperty('--text-color', '#FFFFFF');
+        document.documentElement.style.setProperty('--comment-color', '#FFFFFF80');
         lightMode = false;
     } else {
         document.documentElement.style.setProperty('--background-color', '#FFFFFF');
         document.documentElement.style.setProperty('--text-color', '#000000');
+        document.documentElement.style.setProperty('--comment-color', '#00000080');
         lightMode = true;
     }
 }
